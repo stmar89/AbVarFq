ecd4bfda62bea92cab7df1fa7c7c3c463f2760f0
stmar89 <stefano.marseglia89@gmail.com>
2019-06-13 13:38:42 +0000

    replaced p-Frobenius with q-power

--- OverOrders.m
+++ OverOrders.m
@@ -146,17 +146,17 @@ if not assigned R`MinimalOverOrders then
         if d eq 1 then
           Include(~min_oo, T);
         else
-          p:=Characteristic(F);
-          ppow:=hom<V->V | [mTV((v@@mTV)^p) : v in Basis(V)]>;
-          eigen_vals:=[e[1] : e in Setseq(Eigenvalues(Matrix(ppow)))];
-          if 0 in eigen_vals then //the eigenvalue 0 is a bit faster to handle, so we put it in a separate test.
-              eigen_spaces_z:=Kernel(hom<V->V | [mTV((v@@mTV)^p): v in Basis(V)]>); 
+          q:=#F; //need to use q! not the characterstic of F!
+          qpow:=hom<V->V | [mTV((v@@mTV)^q) : v in Basis(V)]>;
+          eigen_vals:=[e[1] : e in Setseq(Eigenvalues(Matrix(qpow)))];
+          //we treat the 0 eigenvalue separatly because the multiplication test is faster.
+          if 0 in eigen_vals then
+              eigen_spaces_z:=Kernel(hom<V->V | [qpow(v): v in Basis(V)]>); 
               //in this way there are naturally embedded in V
               subs_0:=[ W: W in Submodules(eigen_spaces_z) | Dimension(W) eq 1];
               for W in subs_0 do //dim eq 1
                 wT:=W.1@@mTV;
-                if p eq 2 or IsZero(mTV(wT^2))then
-                //doing this square directly on the finite field level would probably be faster.
+                if q eq 2 or (wT^2) in R then
                 //for p eq 2 being a subspace of the eigenspace garantuees that it is mult closed
                     S:=Order([wT] cat zbR);
                     Include(~pot_min_oo,S);
@@ -164,12 +164,12 @@ if not assigned R`MinimalOverOrders then
                 end if;
               end for;
           end if;
-          eigen_spaces_nz:=[Kernel(hom<V->V | [mTV((v@@mTV)^p)-e*v : v in Basis(V)]>)
+          eigen_spaces_nz:=[Kernel(hom<V->V | [qpow(v)-e*v : v in Basis(V)]>)
                                : e in Exclude(eigen_vals,0)]; //in this way there are naturally embedded in V
           subs_1:=[ W: W in &cat[Submodules(E) : E in eigen_spaces_nz] | Dimension(W) eq 1];
           for W in subs_1 do //dim eq 1
             wT:=W.1@@mTV;
-            if p eq 2 or mTV(wT^2) in W then
+            if q eq 2 or mTV(wT^2) in W then
             //doing this square directly on the finite field level would probably be faster.
             //for p eq 2 being a subspace of the eigenspace garantuees that it is mult closed
                 S:=Order([wT] cat zbR);
