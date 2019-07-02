b9d9515983bf168f2d0cf1a399a621d458669bfa
stmar89 <stefano.marseglia89@gmail.com>
2019-07-02 12:26:02 +0000

    moved the instrinc to compute the reps of the Z-conjugacy matrices outside of IntegralMatrices.m, since it will not be publishded by Eamonn

---
+++ RepsZConjugacy.m
@@ -0,0 +1,21 @@
+freeze;
+
+//////////////////////////////////////////////////////////////
+// Representatives of Z-conjugacy classes of integral matrices
+// with square-free characteristic polynomial
+// Stefano Marseglia, Utrecht University, s.marseglia@uu.nl
+// http://www.staff.science.uu.nl/~marse004/
+//////////////////////////////////////////////////////////////
+
+intrinsic RepresentativesZConjugate( f::RngUPolElt ) -> Seq
+{given a monic square-free polynomial f with integer coefficients it returns a set of representatives of the Z-conjugacy classes of integral matrices with characteristic (and hence also minimal) polynomial f }
+	require BaseRing(f) eq Integers() : "the polynomial doesn't have integer coefficients";
+	require IsSquarefree(f) : "the polynomial is not squarefree";
+	require IsMonic(f) : "the polynomial is not monic";
+
+	A:=AssociativeAlgebra(f);
+	E:=EquationOrder(A);
+	icm:=ICM(E);
+	reps:=[ IdealToMatrix(I) : I in icm ];
+	return reps;
+end intrinsic;
