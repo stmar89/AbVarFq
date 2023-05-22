bef7626339859ec29228c2fa4aaebd332686e4e6
David Roe <roed.math@gmail.com>
2023-05-22 14:50:00 -0400

    Add headers to upload script

--- LMFDB/upload.py
+++ LMFDB/upload.py
@@ -35,8 +35,14 @@ def create_upload_files(basefolders, exclude_gq=[]):
                 pic_prime_gens = F.read().strip()
                 isodata.append(f"{label}:{polcnts[label]}:{pic_prime_gens}\n")
     with open("av_fq_isog.update", "w") as F:
+        _ = F.write(":".join(["label", "principal_polarization_count", "pic_prime_gens"]) + "\n")
+        _ = F.write(":".join(["text", "integer", "integer[]"]) + "\n\n")
         _ = F.write("".join(isodata))
     with open("av_fq_weak_equivalences.update", "w") as F:
+        _ = F.write(":".join(["label", "pic_invs", "pic_basis", "is_product", "product_partition", "is_conjugate_stable", "generator_over_ZFV", "is_Zconductor_sum", "is_ZFVconductor_sum"]) + "\n")
+        _ = F.write(":".join(["text", "integer[]", "integer[]", "boolean", "jsonb", "boolean", "jsonb", "boolean", "boolean"]) + "\n\n")
         _ = F.write("".join(wedata))
     with open("av_fq_pol.update", "w") as F:
+        _ = F.write(":".join(["label", "isog_label", "endomorphism_ring", "isom_label", "degree", "kernel", "aut_group", "geom_aut_group", "is_jacobian", "representative"]) + "\n")
+        _ = F.write(":".join(["text", "text", "text", "text", "smallint", "smallint[]", "text", "text", "boolean", "jsonb"]) + "\n\n")
         _ = F.write("".join(poldata))
